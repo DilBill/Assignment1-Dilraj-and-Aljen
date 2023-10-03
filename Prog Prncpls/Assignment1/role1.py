0d15322d70164469546c4f1b2157c6734a2347fd
DilBil <dilrajbola25@gmail.com>
2023-10-03 14:01:23 -0400

    role1.py v1

--- Prog Prncpls/Assignment1/role1.py
+++ Prog Prncpls/Assignment1/role1.py
@@ -25,36 +25,3 @@ def attrb():
     attrb.append(iq)
     return attrb
 
-'''the challenges function creates the roles own challenges 
-which will then be print depending on the parameter c1 (e.g. if c1 == 1 --> print challenge1)'''
-def challenges(c1):
-    challenge1 = 'you need to get out of your cell,\n you have to steal the keys to the cell from the guard | (you need a 6 or higher to win)'
-    challenge2 = "Great now that your out of your cell you need to get to the security room and take out the guard make sure you don't get caught!(roll more than a 7 or you get caught)"
-    challenge3 = "Your alsmost out, you just need to get over the fence but remeber you need to wait for james's signal(roll more than 8 or you won't get the signal)"
-    if c1 == 1: 
-        print(challenge1) 
-    elif c1 == 2:
-        print(challenge2)
-
-    elif c1 == 3:
-        print(challenge3)
-        
- 
- 
-'''the challengeOutcome function returns the outcome of the challenge based on what you rolled with the diceRoll function'''   
-def challengeOutcome(c1):
-    if c1 == 1: 
-        c1Loss = "the guard caught you trying to make a hole"
-        c1Win = "you got the keys good job!"
-        return c1Win,c1Loss
-        
-    elif c1 == 2:
-        c2Loss = "the guard spotted you"
-        c2Win = "you took the guard down good job!"
-        return c2Win,c2Loss
-        
-    elif c1 == 3:
-        c3Loss = "you were inpatient and got caught"
-        c3Win = "you ESCAPED!"   
-        return c3Win, c3Loss
-    
