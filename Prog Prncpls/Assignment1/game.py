c09064ebcedeabaad0470952ac4d4561359ffd94
DilBil <dilrajbola25@gmail.com>
2023-10-03 09:27:07 -0400

    game.py v1

---
+++ Prog Prncpls/Assignment1/game.py
@@ -0,0 +1,20 @@
+'''Game.py -- this file holds all of the game data and logic so that it can later be accessed by app.py to be implemented '''
+
+# imports
+import time
+import os
+import random
+import role1, role2
+
+def diceRoll():
+    # function to roll the dice
+    return (random.randint(2, 12))
+
+    
+
+    
+    
+
+
+        
+    
