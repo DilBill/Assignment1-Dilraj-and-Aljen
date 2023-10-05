8a1fd67124d2fe49fcb99ab7bdd89cb108010a51
DilBil <dilrajbola25@gmail.com>
2023-10-05 09:52:42 -0400

    game.py v6

--- Prog Prncpls/Assignment1/game.py
+++ Prog Prncpls/Assignment1/game.py
@@ -12,33 +12,40 @@ def diceRoll():
 
     
 '''The quest function allows the user to play the game as the quest function
-gathers information from either role1 or role2 and uses the load and dicrRoll functions
+gathers information from either role1 or role2 and uses the load and diceRoll functions
 this function then uses conditonal statments to implement the game logic'''
 def quest(r1):
-    # function that implments the game logic and challenges
     challengeIdx = 1
     if r1 == '1':
+        # creates a list to hold the attrbs of role10
         attrb = role1.attrb()
         result = 0
         while challengeIdx != 4:
+            # using the challengeIdx we access the challenge for the role
             role1.challenges(challengeIdx)
+            # stores the possible outcomes to a list
             outcome = role1.challengeOutcome(challengeIdx)
             roll = input("\ndo you want to roll the dice or do you want to give up? (roll/giveup) ")
-            
+            # the use if .lower() is to remove all capatials in the string to minimize errors
             if roll.lower() == 'roll':
                 rolled = diceRoll()
-                os.system('cls')
-                
+                # clears the terminal to allow for a clean display and a better user experince
+                os.system('clear')
+                # this conditional statement checks whether the player has won or not by comparing their score
                 if rolled < 6 + challengeIdx - 1:
+                    # we are printing the outcome of the challenge with the list we created earlier
                     print("\n",outcome[1],'Challenge lost')
+                    # we now want to change the attrb value down 1 as the user lost the challenge
                     result = attrb[challengeIdx - 1]
                     result -= 1
                     attrb[challengeIdx - 1] = result
                     print('\nStrength =', attrb[0], '| Courage =',attrb[1], '| IQ =', attrb[2],'\n')
                     challengeIdx += 1
-                    
+                # this conditional statement checks whether the player has won or not by comparing their score    
                 elif rolled >= 6 + challengeIdx:
+                    #print the outcome of the challenge using the outcome list
                     print("\n",outcome[0])
+                    # change the value of the attrb 
                     result = attrb[challengeIdx - 1]
                     result += 1
                     attrb[challengeIdx - 1] = result
@@ -46,11 +53,13 @@ def quest(r1):
                     challengeIdx += 1
             
             elif roll.lower() == 'giveup':
+                # if the user wants to stop then print "Game Over" and break out the loop
                 print('Game Over')
                 break
                 
             elif roll.lower() != 'roll' or roll.lower() != 'giveup':
-                print('\nInvaild Try again\n')
+                # checks if the user inputed a vaild value if not print "Invaild Try Again" and let them try again
+                print('\nInvaild Try Again\n')
             
     elif r1 == '2':
         attrb = role2.attrb()
@@ -62,7 +71,7 @@ def quest(r1):
             
             if roll.lower() == 'roll':
                 rolled = diceRoll()
-                os.system('cls')
+                os.system('clear')
                 
                 if rolled < 6 + challengeIdx - 1:
                     print("\n",outcome[1],'Challenge lost')
